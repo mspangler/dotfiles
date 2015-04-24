a833d607202b93686b31a81774906b9ce28923b2
Mark Spangler <mspangler@gmail.com>
2015-04-24 16:55:04 -0500

    added zsh aliases

---
+++ aliases.zsh
@@ -0,0 +1,9 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+alias cls='clear'
+alias mvn_update='mvn -N versions:update-child-modules;mvn versions:commit'
+alias rake='noglob rake'
+
